6e7f250a64ffcdd678dac72f6a3f0caf4988bd94
Qunhua.Liao <lqh@mingcloud.com>
2020-04-26 11:04:49 +0800

    策略框架

---
+++ quinn-util-constant/src/main/java/com/quinn/util/constant/enums/HttpMethodEnum.java
@@ -0,0 +1,17 @@
+package com.quinn.util.constant.enums;
+
+/**
+ * Http方法枚举类
+ *
+ * @author Qunhua.Liao
+ * @since 2020-04-25
+ */
+public enum HttpMethodEnum {
+
+    // POST
+    POST,
+
+    // GET
+    GET,
+
+}
