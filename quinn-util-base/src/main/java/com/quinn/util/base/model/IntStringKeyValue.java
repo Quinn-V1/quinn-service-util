258412faaacde00e0ec715123b3c7384ece61c7d
Qunhua.Liao <lqh@mingcloud.com>
2020-05-28 13:49:06 +0800

    新增整型-字符串 键值对数据类型

---
+++ quinn-util-base/src/main/java/com/quinn/util/base/model/IntStringKeyValue.java
@@ -0,0 +1,11 @@
+package com.quinn.util.base.model;
+
+/**
+ * 简单的整型-字符串键值对
+ *
+ * @author Qunhua.Liao
+ * @since 2020-04-18
+ */
+public class IntStringKeyValue extends AbstractDefaultKeyValue<Integer, String> {
+
+}
