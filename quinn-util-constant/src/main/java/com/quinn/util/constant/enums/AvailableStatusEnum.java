00918a2f7bef3fcd1f3c4393452f0bf426ee1c01
Qunhua.Liao <lqh@mingcloud.com>
2020-06-18 20:49:29 +0800

    Activiti优化，可独立测试

---
+++ quinn-util-constant/src/main/java/com/quinn/util/constant/enums/AvailableStatusEnum.java
@@ -0,0 +1,20 @@
+package com.quinn.util.constant.enums;
+
+/**
+ * 数据可用状态枚举类
+ *
+ * @author Qunhua.Liao
+ * @since 2020-06-18
+ */
+public enum AvailableStatusEnum {
+
+    // 可用
+    AVAILABLE,
+
+    // 不可用
+    UNAVAILABLE,
+
+    // 全部
+    ALL,
+
+}
