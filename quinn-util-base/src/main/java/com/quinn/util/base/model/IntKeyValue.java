0f34dafce7cf54140afcb05fa6930fca8758da4a
QuinnLiao <bjqh2011@qq.com>
2020-09-16 11:55:25 +0800

    intKeyValue

---
+++ quinn-util-base/src/main/java/com/quinn/util/base/model/IntKeyValue.java
@@ -0,0 +1,11 @@
+package com.quinn.util.base.model;
+
+/**
+ * 简单整型键值对
+ *
+ * @author Qunhua.Liao
+ * @since 2020-04-18
+ */
+public class IntKeyValue extends AbstractDefaultKeyValue<Integer, Integer> {
+
+}
